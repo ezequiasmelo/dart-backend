8e4252fb8cd22dd8f32ccbf953ef8ecd4dd2d57f
ezequiasmelo <ezequias-rm@hotmail.com>
2022-12-28 17:04:19 -0300

    Aula 09

--- bin/infra/custom_server.dart
+++ bin/infra/custom_server.dart
@@ -2,10 +2,11 @@ import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart' as self_io;
 
 class CustomServer {
-  Future<void> initialize(Handler handler) async {
-    String address = 'localhost';
-    int port = 8091;
-
+  Future<void> initialize({
+    required Handler handler,
+    required String address,
+    required int port,
+  }) async {
     await self_io.serve(handler, address, port);
     print('Serving at http://$address:$port');
   }
