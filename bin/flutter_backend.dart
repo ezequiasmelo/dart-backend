f53d8f2cdcced63eaa5ff7b6427fa63db7a2fb6d
ezequiasmelo <ezequias-rm@hotmail.com>
2022-12-28 11:35:57 -0300

    Start project

---
+++ bin/flutter_backend.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_backend/flutter_backend.dart' as flutter_backend;
+
+void main(List<String> arguments) {
+  print('Hello world: ${flutter_backend.calculate()}!');
+}
---
+++ test/flutter_backend_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_backend/flutter_backend.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
