d0500d81a4796293fd78b88ba0f2bbe867533674
ezequiasmelo <ezequias-rm@hotmail.com>
2022-12-29 09:27:44 -0300

    Aula 14

--- bin/flutter_backend.dart
+++ bin/flutter_backend.dart
@@ -2,6 +2,7 @@ import 'package:shelf/shelf.dart';
 import 'apis/blog_api.dart';
 import 'apis/login_api.dart';
 import 'infra/custom_server.dart';
+import 'infra/middleware_interception.dart';
 import 'services/noticia_service.dart';
 import 'utils/custom_env.dart';
 
@@ -13,8 +14,10 @@ void main() async {
       .add(BlogApi(NoticiaService()).handler)
       .handler;
 
-  var handler =
-      Pipeline().addMiddleware(logRequests()).addHandler(cascadeHandler);
+  var handler = Pipeline()
+      .addMiddleware(logRequests())
+      .addMiddleware(MiddlewareInterception().middleware)
+      .addHandler(cascadeHandler);
 
   await CustomServer().initialize(
     handler: handler,
