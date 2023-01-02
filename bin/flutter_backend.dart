63a6d36e5452c323cf5c5e26c4ef30202c9a147c
ezequiasmelo <ezequias-rm@hotmail.com>
2023-01-02 19:46:13 -0300

    Aula 22

--- bin/flutter_backend.dart
+++ bin/flutter_backend.dart
@@ -2,6 +2,7 @@ import 'package:shelf/shelf.dart';
 import 'apis/blog_api.dart';
 import 'apis/login_api.dart';
 import 'infra/custom_server.dart';
+import 'infra/dependency_injector/dependency_injector.dart';
 import 'infra/middleware_interception.dart';
 import 'infra/security/security_service.dart';
 import 'infra/security/security_service_imp.dart';
@@ -11,7 +12,11 @@ import 'utils/custom_env.dart';
 void main() async {
   if (!bool.fromEnvironment('dart.vm.profile')) CustomEnv.fromFile('.env-dev');
 
-  SecurityService securityService = SecurityServiceImp();
+  final _di = DependencyInjector();
+
+  _di.register<SecurityService>(() => SecurityServiceImp(), isSingleton: true);
+
+  var securityService = _di.get<SecurityService>();
 
   var cascadeHandler = Cascade()
       .add(LoginApi(securityService).getHandler())
