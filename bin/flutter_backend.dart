d19dcb7e11aed3671783b6421ebe3e99129a6cae
ezequiasmelo <ezequias-rm@hotmail.com>
2023-01-03 10:37:23 -0300

    Aula 24

--- bin/flutter_backend.dart
+++ bin/flutter_backend.dart
@@ -2,25 +2,18 @@ import 'package:shelf/shelf.dart';
 import 'apis/blog_api.dart';
 import 'apis/login_api.dart';
 import 'infra/custom_server.dart';
-import 'infra/dependency_injector/dependency_injector.dart';
+import 'infra/dependency_injector/injects.dart';
 import 'infra/middleware_interception.dart';
-import 'infra/security/security_service.dart';
-import 'infra/security/security_service_imp.dart';
-import 'services/noticia_service.dart';
 import 'utils/custom_env.dart';
 
 void main() async {
   if (!bool.fromEnvironment('dart.vm.profile')) CustomEnv.fromFile('.env-dev');
 
-  final _di = DependencyInjector();
-
-  _di.register<SecurityService>(() => SecurityServiceImp(), isSingleton: true);
-
-  var securityService = _di.get<SecurityService>();
+  final _di = Injects.initialize();
 
   var cascadeHandler = Cascade()
-      .add(LoginApi(securityService).getHandler())
-      .add(BlogApi(NoticiaService()).getHandler(isSecurity: true))
+      .add(_di<LoginApi>().getHandler())
+      .add(_di<BlogApi>().getHandler(isSecurity: true))
       .handler;
 
   var handler = Pipeline()
